086d447d1b618d60e0ba1a1178b29e59d462bd0c
Lucas Amorim <amorimlucas1998@gmail.com>
2019-08-11 12:02:14 -0300

    Update bsEvaluator to paint bestFlower

--- src/main/java/com/fpa/Main.java
+++ src/main/java/com/fpa/Main.java
@@ -6,6 +6,8 @@ import com.fpa.pollination.FPASolver;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,9 +17,21 @@ public class Main {
         Geometry city = City.getCity1();
         BaseStationEvaluator bsEvaluator = new BaseStationEvaluator(city, K, geoFactory, R);
         FPASolver solver = new FPASolver(20, K * 2, iter, 0.8f, true, bsEvaluator);
+        int i = 0;
+
+        JFrame frame = new JFrame("ERBs") ;
+        frame.getContentPane().add(bsEvaluator);
+        frame.setSize(625, 625);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         while (true) {
             solver.nextIteration();
-            solver.getBestFlower().print();
+            //solver.getBestFlower().print();
+            if (++i % 50 == 0) {
+                bsEvaluator.setBestFlower(solver.getBestFlower());
+                frame.repaint();
+            }
         }
 //        System.out.println("Finished. Best Flower:");
 //        solver.getBestFlower().print();
--- src/main/java/com/fpa/basestation/BaseStationEvaluator.java
+++ src/main/java/com/fpa/basestation/BaseStationEvaluator.java
@@ -1,32 +1,46 @@
 package com.fpa.basestation;
 
+import com.fpa.pollination.Flower;
 import com.fpa.pollination.IFlowerFitnessEvaluator;
+import com.vividsolutions.jts.awt.ShapeWriter;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.util.GeometricShapeFactory;
-import javafx.util.Pair;
 import org.la4j.Vector;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
-public class BaseStationEvaluator implements IFlowerFitnessEvaluator {
+public class BaseStationEvaluator extends JPanel implements IFlowerFitnessEvaluator {
 
     Geometry city;
     int K, R;
     GeometryFactory geoFactory;
+    Polygon container;
+    Flower bestFlower;
 
     public BaseStationEvaluator(Geometry city, int K, GeometryFactory geoFactory, int R) {
         this.city = city;
         this.K = K;
         this.geoFactory = geoFactory;
         this.R = R;
+        container = geoFactory.createPolygon(new Coordinate[]{new Coordinate(0, 0), new Coordinate(625, 0), new Coordinate(625, 625), new Coordinate(0, 625), new Coordinate(0, 0)});
     }
 
     @Override
     public double fitness(Vector x) {
         if (x.length() != K * 2)
             throw new RuntimeException("Não existem coordenadas suficientes no vetor-solução");
+        Geometry tot = asGeometry(x);
+        if (tot == null)
+            return 0;
+        return tot.intersection(city).getArea() / city.getArea();
+    }
+
+    private Geometry asGeometry(Vector x) {
         ArrayList<Coordinate> bsts = new ArrayList<>();
         for (int i = 0; i < x.length(); i += 2) {
             bsts.add(new Coordinate((int)x.get(i), (int)x.get(i + 1)));
@@ -39,9 +53,7 @@ public class BaseStationEvaluator implements IFlowerFitnessEvaluator {
             else
                 tot = tot.union(newStation);
         }
-        if (tot == null)
-            return 0;
-        return tot.intersection(city).getArea() / city.getArea();
+        return tot;
     }
 
     private Geometry createCircle(Coordinate coordinate, int radius) {
@@ -51,4 +63,30 @@ public class BaseStationEvaluator implements IFlowerFitnessEvaluator {
         shape.setNumPoints(32);
         return shape.createCircle();
     }
+
+    public void setBestFlower(Flower f) {
+        bestFlower = f;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        if (bestFlower == null)
+            return;
+        Geometry lastTot = asGeometry(bestFlower.x);
+        if (lastTot == null)
+            return;
+        ShapeWriter sw = new ShapeWriter();
+        Shape linShape = sw.toShape(container.difference(city));
+        g2d.setPaint(new Color(126, 200, 80));
+        g2d.fill(linShape);
+        linShape = sw.toShape(city);
+        g2d.setPaint(new Color(255, 0, 255));
+        g2d.fill(linShape);
+        g2d.setPaint(Color.black);
+        linShape = sw.toShape(lastTot);
+        g2d.fill(linShape);
+    }
+
 }
